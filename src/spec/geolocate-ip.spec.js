2e31d9689ab2ee3a7ce8113272e0e268595e0ff9
Matt <matt@mattjay.com>
2013-11-18 15:29:00 -0600

    basic passing test for geolocate-ip script.

--- src/spec/geolocate-ip.spec.js
+++ src/spec/geolocate-ip.spec.js
@@ -1,3 +1,12 @@
 /**
  * Created by mattjohansen on 11/18/13.
  */
+var geoip = require('../scripts/geolocate-ip.coffee');
+
+describe('geolocation', function() {
+
+  it('should be a function', function(){
+    expect(typeof(geoip)).toBe('function');
+  })
+
+});
