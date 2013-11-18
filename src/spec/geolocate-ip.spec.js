bc87761357d02e2dd5522011be6900d7b0ece4c5
Matt <matt@mattjay.com>
2013-11-18 16:01:06 -0600

    added another test. kind of stuck on how to test if it actually works as expected though.

--- src/spec/geolocate-ip.spec.js
+++ src/spec/geolocate-ip.spec.js
@@ -7,6 +7,18 @@ describe('geolocation', function() {
 
   it('should be a function', function(){
     expect(typeof(geoip)).toBe('function');
-  })
+  });
+
+  it('should call respond when called', function() {
+    var robot = {
+      respond: function(){}
+    };
+
+    spyOn(robot, 'respond');
+    geoip(robot);
+    expect(robot.respond).toHaveBeenCalled();
+  });
+
+  //TODO: Need a way to call robot. Not sure how to do this without access to hubot itself.
 
 });
