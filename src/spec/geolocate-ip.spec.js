109c0cbb7392175b0c81b8bd7e9f3f7cf98b29ff
Matt <matt@mattjay.com>
2013-11-18 18:26:06 -0600

    passing tests for geolocation IP url

--- src/spec/geolocate-ip.spec.js
+++ src/spec/geolocate-ip.spec.js
@@ -2,23 +2,43 @@
  * Created by mattjohansen on 11/18/13.
  */
 var geoip = require('../scripts/geolocate-ip.coffee');
+var HttpClient = require('scoped-http-client');
 
 describe('geolocation', function() {
+  var robot, msg = null;
+
+  beforeEach(function(){
+    robot = {
+      respond: function(regex, callback){},
+      http: function(url){
+        //HttpClient.create(url);
+      }
+    }
+    spyOn(robot, 'respond').andCallThrough();
+    spyOn(robot, 'http').andCallFake(function(){
+      var url = robot.http.mostRecentCall.args[0];
+      var client = HttpClient.create(url);
+      spyOn(client, 'get').andReturn(function(){});
+      return client
+    });
+    geoip(robot);
+    msg = {
+      match: 'geolocate 1.2.3.4'.match(robot.respond.mostRecentCall.args[0]),
+      send: function() {}
+    }
+  });
 
   it('should be a function', function(){
     expect(typeof(geoip)).toBe('function');
   });
 
-  it('should call respond when called', function() {
-    var robot = {
-      respond: function(){}
-    };
-
-    spyOn(robot, 'respond');
-    geoip(robot);
+  it('should generate the correct URL', function() {
     expect(robot.respond).toHaveBeenCalled();
+    console.log(robot.respond.mostRecentCall.args[1]);
+    robot.respond.mostRecentCall.args[1](msg);
+    expect(robot.http).toHaveBeenCalled();
+    expect(robot.http.mostRecentCall.args[0]).toBeDefined();
+    expect(robot.http.mostRecentCall.args[0]).toBe('http://api.hostip.info/get_json.php?ip=1.2.3.4')
   });
-
-  //TODO: Need a way to call robot. Not sure how to do this without access to hubot itself.
 
 });
