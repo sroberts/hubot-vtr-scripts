21a4ebc56c950cf5721e18ed98a9e3efb785b613
Matt <matt@mattjay.com>
2013-12-06 17:01:32 -0600

    created working tests for both new rhodey scripts. updated test.sh to create mock environmental variables for RHODEY_IP and RHODEY_PORT.

--- test.sh
+++ test.sh
@@ -4,4 +4,6 @@ export MYWOT_API_KEY=testing
 export PIPL_API_KEY=testing
 export SHODAN_API_KEY=testing
 export VIRUSTOTAL_API_KEY=testing
+export RHODEY_IP=5.6.7.8
+export RHODEY_PORT=1337
 ./node_modules/jasmine-node/bin/jasmine-node --coffee ./src/spec
